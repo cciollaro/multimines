f2c9e2eeb9ab9efacd3ee9f7f09083497ae8b97d
VGMoose <vgmoose@gmail.com>
2013-11-26 20:17:15 -0500

    added viewing shizz

--- views/index.js
+++ views/index.js
@@ -1,7 +1,26 @@
 var main;
+var main2;
 var lastX = -1;
 var lastY = -1;
 var dragging = false;
+
+var arr1 = makeInitial();
+var arr2 = makeInitial();
+            
+function makeInitial()
+{
+    var matrix = [];
+        for(var i=0; i<10; i++) {
+            matrix[i] = new Array(9);
+            for (var j=0; j<10; j++)
+            {
+                matrix[i][j] = false;
+            }
+    }
+            
+   return matrix;
+
+}
 
 function drawSquares()
 {
@@ -10,27 +29,32 @@ function drawSquares()
     
     //  gradientify(g);     // uncomment for fun!
     
-    for (var x=0; x<this.contents.values.length; x++)
+    for (var x=0; x<this.contents.active.length; x++)
     {
-        if (this.contents.active[x])
-            g.fillStyle="blue";
-        else
-            g.fillStyle="red";
-        
-        g.fillRect(x*30,x*30,30,30); // x, y, width, height
-        
-        g.fillStyle="white";
-        g.font="30px Arial";
-        
-        var off = 7;
-        
-        if (this.contents.values[x].length > 1)
+        for (var y=0; y<this.contents.active[0].length; y++)
         {
-            g.font="20px Arial";
-            off = 0;
+            if (this.contents.active[x][y] == 0)
+                g.fillStyle="blue";
+            else if (this.contents.active[x][y] == 1)
+                g.fillStyle="red";
+            else
+                g.fillStyle="green";
+            
+            g.fillRect(x*30,y*30,30,30); // x, y, width, height
+            
+            g.fillStyle="white";
+            g.font="30px Arial";
+            
+            var off = 7;
+//            
+//            if (this.contents.values[x].length > 1)
+//            {
+//                g.font="20px Arial";
+//                off = 0;
+//            }
+            
+//            g.fillText(this.contents.values[x], x*30+off, x*30+25)
         }
-        
-        g.fillText(this.contents.values[x], x*30+off, x*30+25)
     }
     
     g.fillStyle="black";
@@ -50,24 +74,34 @@ function drawSquares()
 function main()
 {
     main = document.getElementById("main");
+    main2 = document.getElementById("main2");
+
     
     // create arrays of data
     var values = [1,2,3,4,5,6,7,"fish",9];
-    var active = [false, false, false, false, false, false, false, false, false];
+    
+    active = arr1;
+    active2 = arr2;
     
     main.contents = {};
+    main2.contents = {};
+
+    $('body').on('contextmenu', '#main', function(e){ fireClick(e);return false; });
+
     
     main.contents.values = values;
     main.contents.active = active;
     
+    main2.contents.values = values;
+    main2.contents.active = active2;
+    
     main.repaint = drawSquares;
     main.repaint();
     
-    main.addEventListener("mousedown", startDrag);
-    main.addEventListener("mousemove", fireClick);
-    main.addEventListener("mouseup", endDrag);
-    main.addEventListener("mouseout", endDrag);
+    main2.repaint = drawSquares;
+    main2.repaint();
     
+    $('#main').mouseup(fireClick);
     
 }
 
@@ -78,40 +112,26 @@ function clear(g)
 
 function fireClick(event)
 {
-    if (dragging)
+    var x = event.offsetX;
+    var y = event.offsetY;
+    
+    x = Math.ceil(x/30);
+    y = Math.ceil(y/30);
+    
+    if (event.which == 1)
     {
-        var x = event.x;
-        var y = event.y;
-        
-        x -= main.offsetLeft;
-        y -= main.offsetTop;
-        
-        x = Math.ceil(x/30);
-        y = Math.ceil(y/30);
-        
-        if (x == y)
-        {
-            main.contents.active[x-1] = !main.contents.active[x-1];
-        }
-        
-        lastX = x;
-        lastY = y;
-        main.repaint();
-        
-        console.log("got click at ("+x+","+y+")");
-        //connect to the server here
+        main.contents.active[x-1][y-1] = 1;
+        //send flip at x-1, y-1
     }
-}
-
-function startDrag(event)
-{
-    dragging = true;
-    fireClick(event);
-}
-
-function endDrag(event)
-{
-    dragging = false;
+    else if (event.which == 3)
+    {
+        main.contents.active[x-1][y-1] = 2;
+        //send flag at x-1, y-1
+    }
+    
+    lastX = x;
+    lastY = y;
+    main.repaint();
 }
 
 function gradientify(g)
